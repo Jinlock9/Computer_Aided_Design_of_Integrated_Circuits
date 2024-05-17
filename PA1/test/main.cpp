f09cfc6ee221cf8541bf8e649c1a755ccb54f2a9
Jinlock <fkrxkr2@gmail.com>
2024-05-17 18:26:18 +0800

    PA1 test main.cpp remove_space, getGate

--- PA1/test/main.cpp
+++ PA1/test/main.cpp
@@ -1,20 +1,53 @@
+#include "main.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
 int main(int argc, char* argv[]) {
-    std::string line;
-    std::ifstream file (argv[1]);
+    std::string bench = argv[1];
+    std::string value = argv[2];
+    std::ifstream netlist (bench);
 
-    while (getline(file, line)) {
-        if (!line.empty() && !line.find_first_not_of("#")) {
-            std::cout << line << std::endl;
+    std::string raw;
+
+    while (getline(netlist, raw)) {
+        if (!raw.empty() && !raw.find_first_not_of("#")) {
+            std::string line = remove_space(raw);
+            if (line.find("INPUT") == 0 || line.find("input") == 0) {
+                getGate(line);
+            }
+            else if (line.find("OUTPUT") == 0 || line.find("output") == 0) {
+                getGate(line);
+            }
+            else {
+                std::cout << line << std::endl;
+            }
         }
     }
 
-    file.close();
+    netlist.close();
 
     return 0;
+}
+
+auto remove_space(std::string str) -> std::string {
+    std::string filtered = "";
+    for (std::size_t i = 0; i < str.length(); i++) {
+        if(str[i] != ' ') {
+            filtered.push_back(str[i]);
+        }
+    }
+
+    return filtered;
+}
+
+void getGate(std::string str) {
+    std::size_t r = str.find("(");
+    std::size_t l = str.rfind(")");
+    std::string raw = str.substr(r + 1, l - r -1);
+    std::cout << raw << std::endl;
 }
